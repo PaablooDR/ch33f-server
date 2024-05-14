3bdb6ecb26438a9e2faa55dcd97c8b6c6f85d26d
Alex Amer <alex.alcala@inslapineda.cat>
2024-05-14 09:25:31 +0200

    cambios de endpoints de recetas

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -8,3 +8,64 @@ exports.getAllRecipes = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 };
+exports.getSearchedRecipes = async (req, res) => {
+    try {
+        const search = req.query.search || "";
+        const FoundRecipes = await Recipe.find({ title: { $regex: search, $options: "i" } });
+        res.json(FoundRecipes);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+exports.getTopRecipes = async (req, res) => {
+    try {
+        const topRecipes = await Recipe.find({})
+            .sort({ visits: -1 })
+            .limit(5);
+        res.json(topRecipes);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+exports.getFirstRecipes = async (req, res) => {
+    try {
+        const firstRecipes = await Recipe.find({})
+            .limit(10);
+        res.json(firstRecipes);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+exports.getFirstSearchedRecipes = async (req, res) => {
+    try {
+        const search = req.query.search || "";
+        const firstFoundRecipes = await Recipe.find({ title: { $regex: search, $options: "i" } })
+            .limit(50);
+        res.json(firstFoundRecipes);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+exports.getNextRecipes = async (req, res) => {
+    try {
+        const skip = parseInt(req.query.skip) || 0;
+        const nextRecipes = await Recipe.find({})
+            .skip(skip)
+            .limit(5);
+        res.json(nextRecipes);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+exports.getNextSearchedRecipes = async (req, res) => {
+    try {
+        const skip = parseInt(req.query.skip) || 0;
+        const search = req.query.search;
+        const nextFoundRecipes = await Recipe.find({ title: { $regex: search, $options: "i" } })
+            .skip(skip)
+            .limit(5);
+        res.json(nextFoundRecipes);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
--- routes/recipe.js
+++ routes/recipe.js
@@ -3,5 +3,11 @@ const router = express.Router();
 const recipeController = require('../controllers/recipeController');
 
 router.get('/recipes', recipeController.getAllRecipes);
+router.get('/recipes/find', recipeController.getSearchedRecipes);
+router.get('/recipes/top5', recipeController.getTopRecipes);
+router.get('/recipes/first', recipeController.getFirstRecipes);
+router.get('/recipes/nextRecipes', recipeController.getNextRecipes);
+router.get('/recipes/firstsearch', recipeController.getFirstSearchedRecipes);
+router.get('/recipes/nextRecipeSearch', recipeController.getNextSearchedRecipes);
 
 module.exports = router;
